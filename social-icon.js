67e6586d9db122990de50075839f15be8af542e4
Alex Sorni <asorni@gmail.com>
2019-08-12 20:34:39 +0200

    First Commit

---
+++ social-icon.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from 'lit-element';
+
+export class SocialIcon extends LitElement {
+    render() {
+        return html ``;
+    }
+}
+
+customElements.define('social-icon', SocialIcon);
